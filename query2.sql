de1231d52262fa31ab13c264c237d4c248c1401d
Shivraj Kakade <84315160+shivraj-k@users.noreply.github.com>
2021-06-19 17:59:04 +0530

    Update query2.sql
    added constraint

--- query2.sql
+++ query2.sql
@@ -1,1 +1,1 @@
-SELECT books.title, publisher.name FROM books, publisher WHERE publisher.country = 'UK';
+SELECT books.title, publisher.name FROM books, publisher WHERE book.publisher = publisher.id AND publisher.country = 'UK';
